f065dc7c25acbddb9e5bd17cdf5dc5d505cf0cf5
Jacob Shalabi <jshalabi03@gmail.com>
2022-12-31 03:56:30 -0600

    scripts

--- script.sh
+++ script.sh
@@ -6,7 +6,6 @@ docker build -t clang_image .
 
 CONTAINER_ID=$(docker create clang_image:latest)
 docker cp $INPUT_FILE $CONTAINER_ID:/app/tmp/index.cpp
-
 docker start $CONTAINER_ID
 
 docker cp $CONTAINER_ID:/app/tmp/cfg.dot ./output
--- scripts/exec.sh
+++ scripts/exec.sh
@@ -1,5 +1,4 @@
 #!/bin/bash
 
 /app/parse-cfg -f tmp/index.cpp -o tmp/cfg.dot
-
 dot -Tpng tmp/cfg.dot -o tmp/cfg.png
